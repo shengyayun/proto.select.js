cf9798b48014fb6aef422daeeec30b70efd85f57
盛亚运 <shengyy@2345.com>
2015-10-10 18:00:32 +0800

    2.0

--- proto.select.js
+++ proto.select.js
@@ -1,69 +1,88 @@
-function ProtoSelect(id,options){
-     if(typeof ProtoSelect.prototype.InstancePool == "undefined"){
-          ProtoSelect.prototype.InstancePool = {};
+(function(window,undefined){
+     function ProtoSelect(selectId){            
+          var select = document.getElementById(selectId);
+          var proto_select_id = "proto_select_" + selectId;
+          var del = document.getElementById(proto_select_id);
+          if(del != null) 
+               del.parentNode.removeChild(del);               
+          var proto_select = document.createElement("div");
+          proto_select.id = proto_select_id;
+          proto_select.className = "proto_select";
+          select.parentNode.insertBefore(proto_select,select);
+          select.style.display = "none";
+          this.select = select;
+          this.proto_select = proto_select;
      }
-     if(typeof ProtoSelect.prototype.InstancePool[id] != "undefined")
-          return ProtoSelect.prototype.InstancePool[id];
-     if(typeof this.InstancePool == "undefined")
-          throw "excute it with new";
-     var select = document.getElementById(id);    
-     select.className += "proto_select";
-     var html = '<div class="proto_select_current">';
-     html += '<div class="proto_select_current_input" data-value="">&nbsp;</div>';
-     html += '<div class="proto_select_current_arrow">﹀</div>';
-     html += '<div class="proto_select_clear"></div></div>';
-     html += '<ul class="proto_select_list" style="display:none;">';
-     var selected = null;
-     for(var i = 0;i < options.length; i++){
-          var option = options[i];
-          var value = (option.value+"").replace(/"/g,"'");
-          var text = option.text;
-          if(option.hasOwnProperty("selected") && option.selected){
-               selected = option;
+     ProtoSelect.prototype.options = function(){
+          var options =  [];
+          for(var i = 0;i<this.select.options.length;i++){
+               var option = this.select.options[i];
+               options.push({text:option.text,value:option.value});
           }
-          html += '<li class="proto_select_item" data-value="'+value+'">'+text+'</li>';
+          options[this.select.selectedIndex].selected = true;
+          return options;
      }
-     html += '</ul>';
-     select.innerHTML = html;
-     var current = select.getElementsByTagName('div')[0];
-     var input = current.getElementsByTagName('div')[0];
-     var list = select.getElementsByTagName('ul')[0];    
-     var that = this;
-     function save(){
-          that.value = input.getAttribute("data-value");
-          that.text = input.innerHTML;
+     ProtoSelect.prototype.init = function(){
+          var options = this.options();
+          var html = '<div class="proto_select_current">';
+          html += '<div class="proto_select_current_input" data-value="">&nbsp;</div>';
+          html += '<div class="proto_select_current_arrow">﹀</div>';
+          html += '<div class="proto_select_clear"></div></div>';
+          html += '<ul class="proto_select_list" style="display:none;">';
+          this.selected = null;
+          for(var i = 0;i < options.length; i++){
+               var option = options[i];
+               var value = (option.value+"").replace(/"/g,"'");
+               var text = option.text;
+               if(option.hasOwnProperty("selected") && option.selected){
+                    this.selected = option;
+               }
+               html += '<li class="proto_select_item" data-value="'+value+'">'+text+'</li>';
+          }
+          html += '</ul>';
+          this.proto_select.innerHTML = html;
      }
-     this.val = function(){
-          return that.value;
+     ProtoSelect.prototype.bind = function(){
+          function eventBind(a,b,c){
+               if(a.addEventListener)    
+                    a.addEventListener(b,c,false);
+               else if(a.attachEvent)
+                    a.attachEvent("on"+b,c)
+          }
+          var that = this;
+          var proto_select = that.proto_select;
+          var current = proto_select.getElementsByTagName('div')[0];
+          var input = current.getElementsByTagName('div')[0];
+          var list = proto_select.getElementsByTagName('ul')[0];               
+          if(that.selected != null){
+               input.setAttribute("data-value",that.selected.value);
+               input.innerHTML = that.selected.text;
+          }
+          eventBind(that.select,"change",function(){
+               input.setAttribute("data-value",this.value);
+               input.innerHTML = this.options[this.selectedIndex].text;
+          })
+          eventBind(current,"click",function(){
+               list.style.display = list.style.display == "block"?"none":"block";
+          })
+          eventBind(list,"click",function(e){
+               var li = e.target;
+               if(li.className.indexOf("proto_select_item") < 0) return;
+               list.style.display = "none";
+               var value = li.getAttribute("data-value")
+               input.setAttribute("data-value",value);
+               input.innerHTML = li.innerHTML;
+               that.select.value = value;
+               if(list.fireEvent){
+                    list.fireEvent('onchange');
+               } else if(list.onchange){
+                    list.onchange();
+               }
+          })
      }
-     this.change = function(func){
-          that.onChange = func;
+     ProtoSelect.prototype.build = function(){
+          this.init();
+          this.bind();
      }
-     if(selected != null){
-          input.setAttribute("data-value",selected.value);
-          input.innerHTML = selected.text;
-          save();
-     }         
-     function eventBind(a,b,c){
-          if(a.addEventListener)    
-               a.addEventListener(b,c,false);
-          else if(a.attachEvent)
-               a.attachEvent("on"+b,c)
-     }
-     eventBind(current,"click",function(){
-          list.style.display = list.style.display == "block"?"none":"block";
-     })
-     eventBind(list,"click",function(e){
-          var li = e.target;
-          if(li.className.indexOf("proto_select_item") < 0) return;
-          list.style.display = "none";
-          var value = li.getAttribute("data-value")
-          input.setAttribute("data-value",li.getAttribute("data-value"));
-          input.innerHTML = li.innerHTML;
-          save();
-          if(typeof that.onChange != "undefined")
-               that.onChange(value);
-     })
-     ProtoSelect.prototype.InstancePool[id] = this;
-     return ProtoSelect.prototype.InstancePool[id];
-}
+     window.ProtoSelect = ProtoSelect;
+})(window)
